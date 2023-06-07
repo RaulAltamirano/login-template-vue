17a0a4ebe6ae36f8a7721da301fc7d0482d844ef
Pacman <rulomania2011@hotmail.com>
2023-06-07 10:13:49 -0600

    Manin create

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
-import './style.css'
+import '../src/tailwind.css';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { createVuetify } from 'vuetify';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import routes from './routes/routes';
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
