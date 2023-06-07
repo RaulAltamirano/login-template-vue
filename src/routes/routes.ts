e5e52c52035dbe8092604efbc184aeddad94578d
Pacman <rulomania2011@hotmail.com>
2023-06-07 10:17:26 -0600

    Create routes

---
+++ src/routes/routes.ts
@@ -0,0 +1,20 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'login-page',
+    component: () => import('../views/MainPage/MainPage.vue'),
+    children: [
+        { path: '/login', name: 'login-page', component: () => import('../components/LoginComponent/LoginComponent.vue') },
+        { path: '/signup', name: 'signup-page', component: () => import('../components/SignupComponent/SignupComponent.vue') },
+    ],
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('../views/ErrorNotFound/ErrorNotFound.vue'),
+  },
+
+];
+
+export default routes;
