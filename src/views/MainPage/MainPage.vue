038a45c81bd527ac5112aaabf75a5443f1c6d871
Pacman <rulomania2011@hotmail.com>
2023-06-07 10:17:58 -0600

    Create main page

---
+++ src/views/MainPage/MainPage.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainPage',
+});
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
