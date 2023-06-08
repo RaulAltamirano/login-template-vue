378c0f7a944e4232d6b6a4873ac96680a683d55a
Pacman <rulomania2011@hotmail.com>
2023-06-08 16:25:40 -0600

    Cahnge style home page

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -1,58 +1,25 @@
+<script lang="ts" src="./HomePage.ts" />
+
 <template>
-  <div class="home">
-    <v-container fluid>
-      <v-row align="center" justify="center" class="h-100">
-        <v-col cols="12" md="8" lg="6">
-          <v-card class="elevation-12">
-            <v-img
-              gradient="to right, #667eea, #764ba2"
-              height="300"
-            >
-              <v-card-title class="text-center">
-                <h1 class="text-white">Welcome to Our Website</h1>
-              </v-card-title>
-            </v-img>
-            
-            <v-card-text class="px-6 py-8">
-              <p class="text-center text-lg mb-6">
-                Discover the best products and services with our intuitive platform.
-              </p>
-              <v-btn class="mx-auto d-block" color="primary" large rounded>
-                Get Started
-              </v-btn>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <v-row align="center" justify="center" class="h-100">
+    <v-col cols="12" md="8" lg="6" class="text-center">
+      <v-card class="elevation-12">
+        <v-img gradient="to right, #667eea, #764ba2" height="300">
+          <v-card-title class="text-center">
+            <h1 class="text-white">
+              Welcome to Login
+            </h1>
+          </v-card-title>
+        </v-img>
+        <v-card-text class="px-6 py-8">
+          <p class="text-center text-lg mb-6">
+            Hello World!!
+          </p>
+          <v-btn class="mx-auto d-block" color="primary" large rounded>
+            Logout
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
-
-<script>
-export default {
-  name: 'HomePage'
-}
-</script>
-
-<style>
-.home {
-  background-color: #f5f5f5;
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-}
-
-.v-card {
-  border-radius: 8px;
-}
-
-.v-card-title {
-  padding: 24px;
-}
-
-.v-card-text {
-  background-color: #fff;
-  border-bottom-left-radius: 8px;
-  border-bottom-right-radius: 8px;
-}
-</style>
