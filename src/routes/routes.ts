9f0476aad05481af42061c65a04bf6403766274b
Pacman <rulomania2011@hotmail.com>
2023-06-08 16:27:03 -0600

    Change routes file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,20 +1,44 @@
 import { RouteRecordRaw } from 'vue-router';
 
+// Importar componentes de forma asíncrona para una carga más eficiente
+const LoginPage = () => import('../components/LoginComponent/LoginComponent.vue');
+const SignupPage = () => import('../components/SignupComponent/SignupComponent.vue');
+const HomePage = () => import('../components/HomePage/HomePage.vue');
+const ErrorNotFoundPage = () => import('../views/ErrorNotFound/ErrorNotFound.vue');
+
+// Definir rutas como objetos separados para mayor claridad
+const loginRoute: RouteRecordRaw = {
+  path: '/login',
+  name: 'login-page',
+  component: LoginPage,
+};
+
+const signupRoute: RouteRecordRaw = {
+  path: '/signup',
+  name: 'signup-page',
+  component: SignupPage,
+};
+
+const homeRoute: RouteRecordRaw = {
+  path: '/home',
+  name: 'home-page',
+  component: HomePage,
+};
+
+const errorNotFoundRoute: RouteRecordRaw = {
+  path: '/:catchAll(.*)*',
+  component: ErrorNotFoundPage,
+};
+
+// Definir las rutas principales
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    name: 'login-page',
+    name: 'main-page',
     component: () => import('../views/MainPage/MainPage.vue'),
-    children: [
-        { path: '/login', name: 'login-page', component: () => import('../components/LoginComponent/LoginComponent.vue') },
-        { path: '/signup', name: 'signup-page', component: () => import('../components/SignupComponent/SignupComponent.vue') },
-        { path: '/home', name: 'home-page', component: () => import('../components/HomePage/HomePage.vue') },
-    ],
+    children: [loginRoute, signupRoute, homeRoute],
   },
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('../views/ErrorNotFound/ErrorNotFound.vue'),
-  },
+  errorNotFoundRoute,
 ];
 
 export default routes;
