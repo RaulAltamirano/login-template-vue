ac16550a257a55ae187b24d0ee1f51672e45553e
Pacman <rulomania2011@hotmail.com>
2023-06-08 15:38:00 -0600

    Change routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,13 +8,13 @@ const routes: RouteRecordRaw[] = [
     children: [
         { path: '/login', name: 'login-page', component: () => import('../components/LoginComponent/LoginComponent.vue') },
         { path: '/signup', name: 'signup-page', component: () => import('../components/SignupComponent/SignupComponent.vue') },
+        { path: '/home', name: 'home-page', component: () => import('../components/HomePage/HomePage.vue') },
     ],
   },
   {
     path: '/:catchAll(.*)*',
     component: () => import('../views/ErrorNotFound/ErrorNotFound.vue'),
   },
-
 ];
 
 export default routes;
