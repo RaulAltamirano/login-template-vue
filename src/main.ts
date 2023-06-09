681cf0f2de57315055af40d723177e5bd5acb633
Pacman <rulomania2011@hotmail.com>
2023-06-09 15:03:59 -0600

    Add pinia store

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,10 @@ import * as directives from 'vuetify/directives'
 import { createVuetify } from 'vuetify';
 import { createRouter, createWebHashHistory } from 'vue-router';
 import routes from './routes/routes';
+import { createPinia } from 'pinia';
 
-const vuetify = createVuetify({
-  components,
-  directives
-})
-
+const pinia = createPinia();
+const vuetify = createVuetify({ components, directives })
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
@@ -23,4 +21,5 @@ const router = createRouter({
 createApp(App)
   .use(router)
   .use(vuetify)
+  .use(pinia)
   .mount('#app')
