5dd7fc275c13b4f8d79995debed6465744fa6054
Pacman <rulomania2011@hotmail.com>
2023-06-09 22:09:05 -0600

    New method

--- src/composable/useAuth.ts
+++ src/composable/useAuth.ts
@@ -1,5 +1,29 @@
-export const useAuth = () => {
-    return {
+import { storeToRefs } from 'pinia';
+import { useAuthStore } from '../store/store-auth';
+import { postLoginUser } from '../utils/options-auth';
+import { Credentials } from '../interfaces/user-credentials';
+import { useSweetAlert } from './useSweetAlert';
+import { User } from '../interfaces/user-interface';
 
-    };
-}
+export const useAuth = () => {
+	const authStore = useAuthStore();
+	const { } = storeToRefs(authStore);
+	const sweetAlert = useSweetAlert()
+
+	const onLoginUser = async (credentials: Credentials): Promise<User | undefined> => {
+		const { email, pasword } = credentials
+		if (!email || !pasword) {
+			sweetAlert.showErrorAlert(`All field are necsary`)
+			return
+		}
+		const { ok, message, res } = await postLoginUser(credentials);
+		if (!ok) {
+			sweetAlert.showErrorAlert(message)
+			return
+		}
+		return res
+	};
+	return {
+		onLoginUser,
+	};
+};
