ddeb8d664a30689b5361357467aa005f6358f4e3
Pacman <rulomania2011@hotmail.com>
2023-06-09 22:07:58 -0600

    Create VueSweetalert2

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { createApp } from 'vue'
 import '../src/tailwind.css';
-import App from './App.vue'
 
 import 'vuetify/styles'
 
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { createVuetify } from 'vuetify';
+
 import { createRouter, createWebHashHistory } from 'vue-router';
 import routes from './routes/routes';
+
 import { createPinia } from 'pinia';
+import App from './App.vue'
+
+import VueSweetalert2 from 'vue-sweetalert2';
 
 const pinia = createPinia();
 const vuetify = createVuetify({ components, directives })
@@ -22,4 +26,5 @@ createApp(App)
   .use(router)
   .use(vuetify)
   .use(pinia)
+  .use(VueSweetalert2)
   .mount('#app')
