29ec483751a852514457ccb3f7213a0b1c8d5c8e
Pacman <rulomania2011@hotmail.com>
2023-06-12 12:03:40 -0600

    Create login with login User

--- src/components/LoginComponent/LoginComponent.ts
+++ src/components/LoginComponent/LoginComponent.ts
@@ -1,10 +1,12 @@
 import { defineComponent, ref } from "vue"
 import { useAuth } from "../../composable/useAuth"
+import { useRouter } from "vue-router"
 
 export default defineComponent({
 	name: 'login-form',
 	setup() {
 		const { onLoginUser } = useAuth()
+		const router = useRouter()
 		const password = ref<string>('')
 		const email = ref<string>('')
 		const showPassword = ref<boolean>(false)
@@ -12,11 +14,12 @@ export default defineComponent({
 			password,
 			email,
 			showPassword,
-			login() {
-				onLoginUser({
+			async login() {
+				const user = await onLoginUser({
 					email: email.value,
-					pasword: password.value,
+					password: password.value,
 				})
+				if (user) router.push({ name: 'home-page' })
 			}
 		}
 	}
