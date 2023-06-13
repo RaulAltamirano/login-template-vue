9dd8cfa8e397565ec2e418c906865aa10417af09
Pacman <rulomania2011@hotmail.com>
2023-06-13 14:02:48 -0600

    Create method refreshToken

--- src/components/HomePage/HomePage.ts
+++ src/components/HomePage/HomePage.ts
@@ -1,9 +1,18 @@
 import { defineComponent } from "vue"
+import { useAuth } from "../../composable/useAuth"
 
 export default defineComponent({
 	name: 'home-page',
 	setup() {
+		const { onUpdateRefreshToken, getRefreshToken } = useAuth()
 		return {
+			getRefreshToken,
+			refreshToken() {
+				onUpdateRefreshToken()
+			},
+			logout() {
+				console.log('logout');
+			}
 		}
 	}
 }
