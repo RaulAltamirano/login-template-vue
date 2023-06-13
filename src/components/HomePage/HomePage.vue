08ea5a56686af4379214c57e9a0562997ca162cf
Pacman <rulomania2011@hotmail.com>
2023-06-13 14:02:57 -0600

    new style in home page

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -1,22 +1,24 @@
 <script lang="ts" src="./HomePage.ts" />
 
 <template>
-  <v-row align="center" justify="center" class="h-100">
-    <v-col cols="12" md="8" lg="6" class="text-center">
+  <v-row align="center" justify="center" class="h-screen">
+    <v-col cols="12" sm="8" md="6" lg="4">
       <v-card class="elevation-12">
-        <v-img style="  background-image: url('src/assets/waves.svg');
-" gradient="to right, #667eea, #764ba2" height="300">
-          <v-card-title class="text-center">
-            <h1 class="text-white">
-              Welcome to Login
-            </h1>
-          </v-card-title>
-        </v-img>
         <v-card-text class="px-6 py-8">
-          <p class="text-center text-lg mb-6">
-            Hello World!!
-          </p>
-          <v-btn class="mx-auto d-block" color="primary" large rounded>
+          <div class="refresh-token-wrapper">
+            <p class="refresh-token-label">Refresh Token:</p>
+            <p class="refresh-token">
+              {{ getRefreshToken }}
+            </p>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn block color="secondary" large rounded @click="refreshToken">
+            Refresh Token
+          </v-btn>
+        </v-card-actions>
+        <v-card-text>
+          <v-btn block color="primary" large rounded @click="logout">
             Logout
           </v-btn>
         </v-card-text>
@@ -24,3 +26,39 @@
     </v-col>
   </v-row>
 </template>
+
+<style scoped>
+.v-card {
+  border-radius: 8px;
+}
+
+.v-img {
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+}
+
+.text-white {
+  color: white;
+}
+
+.refresh-token-wrapper {
+  margin-bottom: 16px;
+  text-align: center;
+}
+
+.refresh-token-label {
+  font-weight: bold;
+  color: #888;
+}
+
+.refresh-token {
+  font-size: 18px;
+  color: #555;
+  word-break: break-all;
+}
+
+
+.animated {
+  animation: slideIn 0.3s ease-out;
+}
+</style>
