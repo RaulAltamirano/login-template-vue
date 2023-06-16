bfb9042db7fcb7cf29718e64b8d605c6aa0106f9
Pacman <rulomania2011@hotmail.com>
2023-06-15 21:44:53 -0600

    Change functions

--- src/composable/useAuth.ts
+++ src/composable/useAuth.ts
@@ -50,29 +50,51 @@ export const useAuth = () => {
 			sweetAlert.showErrorAlert('An error occurred while logging in');
 		}
 	};
-	const onUpdateRefreshToken = async () => {
-		authStore.setLoadingRefreshToken(true)
-		const token = await useToken.getTokens();
-		if (!token)
-			return sweetAlert.showErrorAlert('The token was not found');
+	const refreshAccessToken = async (refreshToken: string) => {
 		try {
-			const { ok, message, res } = await postRefreshToken(token.refreshToken)
-			if (!ok)
-				sweetAlert.showErrorAlert(message);
-			if (!res)
-				sweetAlert.showErrorAlert('Unable to retrieve the refresh token');
+			const { ok, message, res } = await postRefreshToken(refreshToken);
+			if (!ok) {
+				throw new Error(message);
+			}
+			if (!res) {
+				throw new Error('Unable to retrieve the refresh token');
+			}
+			return res.token.accessToken;
+		} catch (error) {
+			console.error('Error refreshing access token:', error);
+			throw new Error('An error occurred while refreshing the access token');
+		}
+	};
+	
+	const updateRefreshToken = async () => {
+		try {
+			authStore.setLoadingRefreshToken(true);
+			const token = await useToken.getTokens();
+			if (!token) {
+				throw new Error('The token was not found');
+			}
+			const newAccessToken = await refreshAccessToken(token.refreshToken);
 			await useToken.storeTokens({
-				accessToken: res.token.accessToken,
-				refreshToken: res.token.refreshToken
-			}); console.info('refresh token update');
+				accessToken: newAccessToken,
+				refreshToken: token.refreshToken,
+			});
+			console.info('Refresh token updated');
 		} catch (error) {
 			console.error('Error updating refresh token:', error);
-			sweetAlert.showErrorAlert('An error occurred while updating the refresh token');
+			throw new Error('An error occurred while updating the refresh token');
 		} finally {
-			authStore.setLoadingRefreshToken(false)
+			authStore.setLoadingRefreshToken(false);
 		}
-	}
-
+	};
+	
+	const onUpdateRefreshToken = async () => {
+		try {
+			await updateRefreshToken();
+		} catch (error) {
+			sweetAlert.showErrorAlert(error);
+		}
+	};
+	
 	return {
 		// Methods
 		onLoginUser,
