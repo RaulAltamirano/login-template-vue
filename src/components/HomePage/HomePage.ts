fe45c247dfd02733fcfdf5acc263587aafc6586f
Pacman <rulomania2011@hotmail.com>
2023-06-20 22:24:49 -0600

    Update home page

--- src/components/HomePage/HomePage.ts
+++ src/components/HomePage/HomePage.ts
@@ -1,9 +1,11 @@
 import { defineComponent } from "vue"
 import { useAuth } from "../../composable/useAuth"
+import { useRouter } from "vue-router"
 
 export default defineComponent({
 	name: 'home-page',
 	setup() {
+		const router = useRouter()
 		const {
 			onUpdateRefreshToken,
 			getRefreshToken,
@@ -17,6 +19,9 @@ export default defineComponent({
 			},
 			logout() {
 				console.log('logout');
+			},
+			toProtectedRoute() {
+				router.push({ name: 'protected-route' })
 			}
 		}
 	}
