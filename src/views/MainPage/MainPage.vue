c8c0cb4213a1535055b4a2163d8b42a8db47f854
Pacman <rulomania2011@hotmail.com>
2023-06-20 22:28:35 -0600

    Update main

--- src/views/MainPage/MainPage.vue
+++ src/views/MainPage/MainPage.vue
@@ -1,10 +1,4 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'MainPage',
-});
-</script>
+<script lang="ts" src="./MainPage"/>
 
 <template>
   <v-app>
