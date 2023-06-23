7298f98bcd0e42798a12377d55997b8db8084d28
Pacman <rulomania2011@hotmail.com>
2023-06-23 13:28:18 -0600

    update main page

--- src/views/MainPage/MainPage.ts
+++ src/views/MainPage/MainPage.ts
@@ -1,5 +1,16 @@
-import { defineComponent } from 'vue';
 
+import { defineComponent, ref } from 'vue';
+import AuthenticatingPage from '../../components/AuthenticatingPage/AuthenticatingPage.vue';
 export default defineComponent({
-  name: 'MainPage',
-});
+  name: 'authenticating-page',
+  components: { AuthenticatingPage },
+  setup() {
+    const isAuth = ref<boolean>(false)
+    // const {checkStatusLogin}= useAuth()
+    return {
+      isAuth,
+      // checkStatusLogin
+    }
+  }
+}
+)
