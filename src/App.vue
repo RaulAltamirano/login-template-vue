1d6c6dad493e5b25f15b96262e86dbda81793414
Pacman <rulomania2011@hotmail.com>
2023-06-22 21:35:30 -0600

    Changes in app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
-<script setup lang="ts">
-import MainPage from './views/MainPage/MainPage.vue';
-</script>
 
 <template>
   <MainPage />
+  <RouterView />
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MainPage from './views/MainPage/MainPage.vue';
+import AuthenticatingPage from './components/AuthenticatingPage/AuthenticatingPage.vue';
+export default defineComponent({
+  name: 'authenticating-page',
+  components: { AuthenticatingPage },
+  setup() {
+    return {
+    }
+  }
+}
+)
+</script>
