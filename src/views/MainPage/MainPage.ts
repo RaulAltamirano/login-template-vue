93be70abd3a6ddf56e3ad8355689da8b61ee848a
Pacman <rulomania2011@hotmail.com>
2023-06-26 08:44:59 -0600

    Handler refresh token access

--- src/views/MainPage/MainPage.ts
+++ src/views/MainPage/MainPage.ts
@@ -1,19 +1,30 @@
 
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
 
-import AuthenticatingPage from '../AuthenticatingPage/AuthenticatingPage';
+import AuthenticatingPage from '../AuthenticatingPage/AuthenticatingPage.vue';
 
 import { useAuth } from '../../composable/useAuth';
+import { useRefreshTokenStorage } from '../../composable/useToken';
 
 export default defineComponent({
   name: 'authenticating-page',
   components: { AuthenticatingPage },
   setup() {
+
     const {
       checkStatusLogin,
       getCurrentLoginUser
     } = useAuth()
-    checkStatusLogin()
+    
+    const {
+      initIndexedDB
+    } = useRefreshTokenStorage()
+    
+    onMounted(async () => {
+      await initIndexedDB()
+      await checkStatusLogin();
+    });
+    
     return {
       getCurrentLoginUser,
     }
