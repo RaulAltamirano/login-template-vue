be0ca8c05f35ab27ec283a6159c4c9796a1c56d8
Pacman <rulomania2011@hotmail.com>
2023-06-27 17:03:30 -0600

    update api service

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -9,29 +9,48 @@ const api: AxiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
 const { getTokens } = useRefreshTokenStorage()
-// const { onUpdateRefreshToken } = useAuth()
+// api.interceptors.response.use(
+//   (response: AxiosResponse) => {
+//     return response.data;
+//   },
+//   (error: AxiosError) => {
+//     const errorResponse = error.response;
+//     if (errorResponse) {
+//       console.error('Response Error:', errorResponse);
+//       if (errorResponse.status === 401) {
+//         onUpdateRefreshToken()
+//       }
+//     } else if (error.request) {
+//       console.error('Request Error:', error.request);
+//     } else {
+//       console.error('Error:', error.message);
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+
 
 api.interceptors.response.use(
-  (response: AxiosResponse) => {
-    return response.data;
-  },
-  (error: AxiosError) => {
-    const errorResponse = error.response;
-    if (errorResponse) {
-      console.error('Response Error:', errorResponse);
-      if (errorResponse.status === 401) {
-        // onUpdateRefreshToken()
-      }
-    } else if (error.request) {
-      console.error('Request Error:', error.request);
-    } else {
-      console.error('Error:', error.message);
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const { onUpdateRefreshToken } = useAuth()
+      await getTokens()
+      await onUpdateRefreshToken()
+    }
+    if (error.response.status === 500) {
+      // retry the request after a delay
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return axios(originalRequest);
     }
     return Promise.reject(error);
   }
 );
-
 
 api.interceptors.request.use(async (config) => {
   const tokens = await getTokens();
