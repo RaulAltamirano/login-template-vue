ade426a8af9b20be5fcf50609baa13331eafb9e6
Pacman <rulomania2011@hotmail.com>
2023-06-27 17:02:40 -0600

    Create protected route

--- src/components/ProtectedRoute/ProtectedRoute.ts
+++ src/components/ProtectedRoute/ProtectedRoute.ts
@@ -1,10 +1,31 @@
-import { defineComponent } from "vue"
+import { defineComponent, onMounted, ref } from "vue"
+import { useRouter } from "vue-router";
 
 export default defineComponent({
 	name: 'protected-route',
 	setup() {
+		const router = useRouter()
+		const animated = ref(false);
+		const user = {
+			name: 'John Doe',
+			email: 'johndoe@example.com',
+			avatar: 'src/assets/avatar.svg'
+		};
+
+		onMounted(() => {
+			animated.value = true;
+		});
+
 		return {
-		}
-	}
+			animated,
+			user,
+			goHome(){
+				router.push({ name: 'home-page' })
+			},
+			logout(){
+				router.push({ name: 'home-page' })
+			}
+		};
+	},
 }
 )
