7687d636ef5cd6959816cb7a8eee0cd9a0c4d90b
Pacman <rulomania2011@hotmail.com>
2023-06-30 08:25:25 -0600

    File moved  another folder

---
+++ src/modules/auth/composables/useAuth.ts
@@ -0,0 +1,135 @@
+import { computed } from 'vue';
+import { storeToRefs } from 'pinia';
+
+import {
+	getLogoutUser,
+	postCheckStatusLogin,
+	postLoginUser,
+	postRefreshToken
+} from '../helper/options-auth';
+
+import { Credentials, Token, User } from '../interfaces';
+
+import { useSweetAlert } from '../../shared/composable/useSweetAlert';
+import { useRefreshTokenStorage } from './useToken';
+import { useAuthStore } from '../store/store-auth';
+
+
+export const useAuth = () => {
+	const authStore = useAuthStore();
+	const { } = storeToRefs(authStore);
+
+	const useToken = useRefreshTokenStorage()
+	const sweetAlert = useSweetAlert()
+
+	const onLoginUser = async (credentials: Credentials): Promise<User | undefined> => {
+		const { email, password } = credentials;
+		if (!email || !password) {
+			sweetAlert.showErrorAlert(`All fields are necessary`);
+			return;
+		}
+		try {
+			const { ok, message, res } = await postLoginUser(credentials);
+			if (!ok) {
+				sweetAlert.showErrorAlert(message);
+				return;
+			}
+			if (!res) {
+				sweetAlert.showErrorAlert('The user was not found');
+				return;
+			}
+			if (!res.token) {
+				sweetAlert.showErrorAlert('The token was not found');
+				return;
+			}
+			await updateTokens(res.token)
+			delete res.token
+			authStore.setLoginUser(res)
+			return res;
+		} catch (error) {
+			console.error('Error logging in:', error);
+			sweetAlert.showErrorAlert('An error occurred while logging in');
+		}
+	};
+
+	const updateTokens = async (token: Token) => {
+		const { accessToken, refreshToken } = token
+		authStore.setRefreshToken(refreshToken)
+		await useToken.storeTokens({ accessToken, refreshToken });
+	}
+	const refreshAccessToken = async (refreshToken: string): Promise<Token> => {
+		try {
+			const { ok, message, res } = await postRefreshToken(refreshToken);
+			if (!ok) {
+				throw new Error(message);
+			}
+			if (!res) {
+				throw new Error('Unable to retrieve the refresh token');
+			}
+			return res;
+		} catch (error) {
+			console.error('Error refreshing access token:', error);
+			throw new Error('An error occurred while refreshing the access token');
+		}
+	};
+
+	const updateRefreshToken = async () => {
+		try {
+			authStore.setLoadingRefreshToken(true);
+			const token = await useToken.getTokens();
+			if (!token) {
+				throw new Error('The token was not found');
+			}
+			const newTokens = await refreshAccessToken(token.refreshToken);
+			await updateTokens(newTokens)
+		} catch (error) {
+			console.error('Error updating refresh token:', error);
+			throw new Error('An error occurred while updating the refresh token');
+		} finally {
+			authStore.setLoadingRefreshToken(false);
+		}
+	};
+
+	const onUpdateRefreshToken = async () => {
+		try {
+			await updateRefreshToken();
+		} catch (error) {
+			sweetAlert.showErrorAlert(error);
+		}
+	};
+	const checkStatusLogin = async (): Promise<User | undefined> => {
+		try {
+			const token = await useToken.getTokens();
+			if (!token) {
+				throw new Error('Token not found. Please log in.');
+			}
+			const { refreshToken } = token;
+			const { ok, res } = await postCheckStatusLogin(refreshToken);
+			if (!ok || !res) {
+				throw new Error('Authentication check failed. Please log in again.');
+			}
+			authStore.setRefreshToken(res.refreshToken)
+			authStore.setLoginUser(res);
+			return res;
+		} catch (error) {
+			console.error('Error checking authentication:', error);
+			return;
+		}
+	};
+	const logoutUser = async (): Promise<boolean> => {
+		const { ok } = await getLogoutUser()
+		return ok
+	}
+
+	return {
+		// Methods
+		onLoginUser,
+		logoutUser,
+		checkStatusLogin,
+		onUpdateRefreshToken,
+		// Getters
+		getRefreshToken: computed(() => authStore.currentRefreshTokenState),
+		getLoadingRefreshToken: computed(() => authStore.loadingRefreshTokenState),
+		getCurrentLoginUser: computed(() => authStore.currentLoginUserState),
+	};
+};
