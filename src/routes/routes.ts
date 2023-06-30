f5ea06393a7b9df6639e04dd520855b2bef47c45
Pacman <rulomania2011@hotmail.com>
2023-06-30 08:27:32 -0600

    Update imports

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,15 @@
 import { RouteRecordRaw } from 'vue-router';
 
-import isAuthenticatedGuard from '../guards/auth-guard'
+import isAuthenticatedGuard from '../modules/auth/guards/auth-guard';
+
 // Importar componentes de forma asíncrona para una carga más eficiente
-const LoginPage = () => import('../components/LoginComponent/LoginComponent.vue');
-const SignupPage = () => import('../components/SignupComponent/SignupComponent.vue');
+const LoginPage = () => import('../modules/auth/components/LoginComponent/LoginComponent.vue');
+const SignupPage = () => import('../modules/auth/components/SignupComponent/SignupComponent.vue');
 const HomePage = () => import('../components/HomePage/HomePage.vue');
 const ErrorNotFoundPage = () => import('../views/ErrorNotFound/ErrorNotFound.vue');
-const ProtectedRoute = () => import('../components/ProtectedRoute/ProtectedRoute.vue');
+const ProtectedRoute = () => import('../modules/protected/components/ProtectedRoute/ProtectedRoute.vue');
+
+
 // Definir rutas como objetos separados para mayor claridad
 const loginRoute: RouteRecordRaw = {
   path: '/login',
