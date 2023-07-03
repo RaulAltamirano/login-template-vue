ee8b0dc7887625f526c1a8f4e97c4ae5a0bebae3
Pacman <rulomania2011@hotmail.com>
2023-07-03 14:38:43 -0600

    Update auth guard

--- src/modules/auth/guards/auth-guard.ts
+++ src/modules/auth/guards/auth-guard.ts
@@ -2,20 +2,26 @@ import { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
 
 import { useAuth } from '../composables/useAuth';
 import { useRefreshTokenStorage } from '../composables/useToken';
+// import { AuthenticationStatus } from '../interfaces';
 
-const isAuthenticatedGuard = async (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
-    const { checkStatusLogin } = useAuth();
-    const { initIndexedDB } = useRefreshTokenStorage();
+const isAuthenticatedGuard = async (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  const { checkStatusLogin, getStatusLogin } = useAuth();
+  const { initIndexedDB } = useRefreshTokenStorage();
 
-    try {
-        await initIndexedDB();
-        const isLogged = await checkStatusLogin();
-        if (isLogged) next();
-        else next({ name: 'login-page' });
-    } catch (error) {
-        console.error('Error in isAuthenticatedGuard:', error);
-        next({ name: 'error-page' });
-    }
+  try {
+    await initIndexedDB();
+    await checkStatusLogin();
+    (getStatusLogin.value)
+      ? next()
+      : next({ name: 'login-page' });
+  } catch (error) {
+    console.error('Error in isAuthenticatedGuard:', error);
+    next({ name: 'error-page' });
+  }
 };
 
 export default isAuthenticatedGuard;
