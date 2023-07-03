1d2f780e79b6dec0c1b03e70c8d0942d6f88c7e0
Pacman <rulomania2011@hotmail.com>
2023-07-03 14:39:15 -0600

    Create barrel file

--- src/modules/auth/interfaces/index.ts
+++ src/modules/auth/interfaces/index.ts
@@ -1,3 +1,4 @@
+export { AuthenticationStatus } from './status-authentication';
 export type { Credentials } from '../interfaces/user-credentials';
 export type { User } from '../interfaces/user-interface';
 export type { Token } from '../interfaces/user-token';
