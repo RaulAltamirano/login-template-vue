9f54080f7a46fe5a12a01707eb6bacf70051c6c3
Pacman <rulomania2011@hotmail.com>
2023-09-18 21:37:50 -0600

    [UPDATE] Update store subscription

---
+++ src/modules/subscription/store/subscription-store.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia';
+interface SubscriptionStore {
+
+}
+
+export const useSubscriptionStore = defineStore('subscription', {
+    state: (): SubscriptionStore => ({
+
+    }),
+    actions: {
+
+    },
+    getters: {
+    },
+});
