601c76c3439e8200a45c83fd100e66594ff5ca88
Pacman <rulomania2011@hotmail.com>
2023-09-22 21:18:52 -0600

    [UPDATE] Update styles

--- src/modules/subscription/views/SubscriptionPage/SubscriptionPage.vue
+++ src/modules/subscription/views/SubscriptionPage/SubscriptionPage.vue
@@ -1,31 +1,62 @@
 <script lang="ts" src="./SubscriptionPage.ts"/>
 
 <template>
-	<v-row align="center" justify="center" class="h-screen">
-		<v-row align="center" justify="center" class="pa-2 ma-2">
-			<v-col cols="12" class="ma-8">
-				<h2 class="text-h2 write-effect" justify="center" align="center">
-					Flexible Plans
-				</h2>
-				<p class="text-caption primary-text write-effect" color="primary" align="center">
-					Choose a plan that works best for you and your team.
-				</p>
-			</v-col>
-			<v-col cols="12" md="4">
-				<PlanCard title="Basic" price="10" :features="basic" />
-			</v-col>
-			<v-col cols="12" md="4">
-				<PlanCard title="Startup" price="24" popular :features="startup" />
-			</v-col>
-			<v-col cols="12" md="4">
-				<PlanCard title="Enterprise" price="35" :features="enterprise" />
-			</v-col>
-		</v-row>
+	<v-row justify="center" align-content="center" class="h-screen">
+		<div class="background-image">
+			<v-row align="center" justify="center" class="pa-2 ma-2">
+				<v-col cols="12" class="ma-8 text-center">
+					<h2 class="text-h2 write-effect" style="color: #ffffff;" justify="center" align="center">
+						Flexible Plans
+					</h2>
+					<p class="text-caption primary-text write-effect" color="primary" align="center">
+						Choose a plan that works best for you and your team.
+					</p>
+				</v-col>
+				<v-col cols="12" md="4">
+					<PlanCard title="Basic" price="10" :features="basic" />
+				</v-col>
+				<v-col cols="12" md="4">
+					<PlanCard title="Startup" price="24" popular :features="startup" />
+				</v-col>
+				<v-col cols="12" md="4">
+					<PlanCard title="Enterprise" price="35" :features="enterprise" />
+				</v-col>
+			</v-row>
+		</div>
 	</v-row>
 </template>
 
 <style scoped>
+.background-image {
+	background-image: url('src/assets/example.svg');
+	background-size: cover;
+	background-position: center;
+	position: relative;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	opacity: 0;
+	/* Inicialmente establece la opacidad en 0 para ocultar la imagen */
+	animation: fadeIn 1s ease-in-out forwards;
+	/* Define la animación de entrada */
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.text-center {
+	text-align: center;
+}
+
 .write-effect {
+	color: aliceblue;
 	overflow: hidden;
 	white-space: nowrap;
 	width: 0;
