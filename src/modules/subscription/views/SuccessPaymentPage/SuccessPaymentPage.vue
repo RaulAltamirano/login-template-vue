8699176d94d995b13c69b2a1e7daa8bda77d8ef7
Pacman <rulomania2011@hotmail.com>
2023-09-25 21:39:19 -0600

    [CREATE] Create succes payment

--- src/modules/subscription/views/SuccessPaymentPage/SuccessPaymentPage.vue
+++ src/modules/subscription/views/SuccessPaymentPage/SuccessPaymentPage.vue
@@ -1,71 +1,77 @@
 <template>
-    <div class="min-h-screen flex items-center justify-center">
-      <div class="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
-        <h1 class="text-2xl font-semibold mb-4">Detalles del pago</h1>
-        <form @submit.prevent="realizarPago">
-          <!-- Campos de información del usuario y tarjeta -->
-          <div class="mb-4">
-            <label for="nombre" class="block text-gray-600 text-sm font-medium mb-2">Nombre</label>
-            <input type="text" id="nombre" v-model="nombre" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300">
-          </div>
-          <div class="mb-4">
-            <label for="tarjeta" class="block text-gray-600 text-sm font-medium mb-2">Número de tarjeta</label>
-            <input type="text" id="tarjeta" v-model="numeroTarjeta" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300">
-          </div>
-          <div class="mb-4">
-            <label for="caducidad" class="block text-gray-600 text-sm font-medium mb-2">Fecha de caducidad</label>
-            <input type="text" id="caducidad" v-model="caducidad" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300">
-          </div>
-          <div class="mb-4">
-            <label for="cvv" class="block text-gray-600 text-sm font-medium mb-2">CVV</label>
-            <input type="text" id="cvv" v-model="cvv" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300">
-          </div>
-          
-          <!-- Botón de pago -->
-          <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300">Pagar</button>
-        </form>
+  <div class="p-4">
+    <h1 class="text-2xl font-semibold mb-4">Detalles de la Factura</h1>
+    
+    <!-- Información de la factura -->
+    <div class="bg-white shadow-md rounded p-4 mb-4">
+      <div class="flex justify-between mb-2">
+        <span>Fecha de Emisión:</span>
+        <span>15 de Septiembre, 2023</span>
       </div>
-      
-      <!-- Animación de confirmación -->
-      <transition name="fade" mode="out-in">
-        <div v-if="pagoRealizado" class="bg-green-200 text-green-700 p-4 mt-4 rounded-lg shadow-md max-w-md w-full">
-          <p class="text-xl font-semibold">¡Pago realizado con éxito!</p>
-          <p class="mt-2">Gracias por su compra, {{ nombre }}.</p>
-        </div>
-      </transition>
+      <div class="flex justify-between mb-2">
+        <span>Número de Factura:</span>
+        <span>#12345</span>
+      </div>
+      <div class="flex justify-between mb-2">
+        <span>Cliente:</span>
+        <span>Nombre del Cliente</span>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        nombre: '',
-        numeroTarjeta: '',
-        caducidad: '',
-        cvv: '',
-        pagoRealizado: false,
-      };
+
+    <!-- Detalles de la factura -->
+    <div class="bg-white shadow-md rounded p-4">
+      <table class="w-full">
+        <thead>
+          <tr>
+            <th class="text-left">Descripción</th>
+            <th class="text-right">Cantidad</th>
+            <th class="text-right">Precio Unitario</th>
+            <th class="text-right">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Producto 1</td>
+            <td class="text-right">2</td>
+            <td class="text-right">$50.00</td>
+            <td class="text-right">$100.00</td>
+          </tr>
+          <tr>
+            <td>Producto 2</td>
+            <td class="text-right">1</td>
+            <td class="text-right">$75.00</td>
+            <td class="text-right">$75.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <!-- Total de la factura -->
+    <div class="mt-4">
+      <div class="flex justify-between">
+        <span class="text-xl font-semibold">Total:</span>
+        <span class="text-xl font-semibold">$175.00</span>
+      </div>
+    </div>
+
+    <!-- Botón de pago -->
+    <div class="mt-6">
+      <v-btn color="primary" @click="realizarPago">Realizar Pago</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    realizarPago() {
+      // Aquí puedes agregar la lógica para procesar el pago
+      alert("Pago realizado con éxito");
     },
-    methods: {
-      realizarPago() {
-        // Aquí puedes agregar lógica para procesar el pago (por ejemplo, con una API).
-        // Cuando el pago se realiza correctamente, establece `pagoRealizado` en true.
-        // En este ejemplo, simplemente lo estableceremos en true después de un breve retraso simulado.
-        setTimeout(() => {
-          this.pagoRealizado = true;
-        }, 2000);
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  /* Estilo de la animación de transición */
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity 0.5s;
-  }
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
-  </style>
+  },
+};
+</script>
+
+<style scoped>
+/* Puedes agregar estilos personalizados aquí si es necesario */
+</style>
