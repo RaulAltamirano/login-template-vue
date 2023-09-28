f9ad0e3d370b02b4521fa76f5ead09894f23d5a4
Pacman <rulomania2011@hotmail.com>
2023-09-27 22:05:06 -0600

    [UDPATE] Update success payment

--- src/modules/subscription/views/SuccessPaymentPage/SuccessPaymentPage.vue
+++ src/modules/subscription/views/SuccessPaymentPage/SuccessPaymentPage.vue
@@ -1,18 +1,3 @@
 <template>
  
 </template>
-
-<script>
-export default {
-  methods: {
-    realizarPago() {
-      // Aquí puedes agregar la lógica para procesar el pago
-      alert("Pago realizado con éxito");
-    },
-  },
-};
-</script>
-
-<style scoped>
-/* Puedes agregar estilos personalizados aquí si es necesario */
-</style>
