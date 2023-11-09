968b10b4e8c37c36c3b5f90589e337e5249093ab
Pacman <rulomania2011@hotmail.com>
2023-11-08 21:31:13 -0600

    [UPDATE] Update routes index

--- src/modules/subscription/routes/index.ts
+++ src/modules/subscription/routes/index.ts
@@ -2,7 +2,6 @@ import { RouteRecordRaw } from "vue-router";
 
 const SubscriptionRoute = () => import(/*webpackChunkName : "Subscription-page"*/'../../subscription/views/SubscriptionPage/SubscriptionPage.vue');
 const PaymentMethodRoute = () => import(/*webpackChunkName : "Payment-Method-page"*/'../../subscription/views/PaymentMethods/PaymentMethods.vue');
-const SuccessPaymentRoute = () => import(/*webpackChunkName : "Success-Payment-page"*/'../../subscription/views/SuccessPaymentPage/SuccessPaymentPage.vue');
 
 const subscriptionPage: RouteRecordRaw = {
     path: '/subscription',
@@ -15,14 +14,9 @@ const paymentMethodPage: RouteRecordRaw = {
     name: 'payment-method-page',
     component: PaymentMethodRoute,
 };
-const successPaymentPage: RouteRecordRaw = {
-    path: '/success-payment',
-    name: 'success-payment',
-    component: SuccessPaymentRoute,
-};
 
 export default {
     name: 'subscription',
     component: () => import(/*webpackChunkName : "Main-page"*/'../../../views/MainPage/MainPage.vue'),
-    children: [subscriptionPage, paymentMethodPage, successPaymentPage],
+    children: [subscriptionPage, paymentMethodPage],
 }
