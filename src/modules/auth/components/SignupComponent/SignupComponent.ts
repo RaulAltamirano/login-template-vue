564535162ab615086f513562e97fd10bf1547103
Pacman <rulomania2011@hotmail.com>
2024-03-07 21:28:26 -0600

    [UPDATE] Add new ref by signup

--- src/modules/auth/components/SignupComponent/SignupComponent.ts
+++ src/modules/auth/components/SignupComponent/SignupComponent.ts
@@ -1,9 +1,20 @@
-import { defineComponent } from "vue"
+import { defineComponent, ref } from "vue"
 
 export default defineComponent({
 	name: 'signup-form',
 	setup() {
+		const name = ref('')
+		const email = ref('')
+		const password = ref('')
+		const confirmPassword = ref('')
 		return {
+			name,
+			email,
+			password,
+			confirmPassword,
+			signup(){
+				console.log('signup');
+			}
 		}
 	}
 }
